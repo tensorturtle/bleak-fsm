7cb7534eb92ed3aacad137b251921cb8bff0bdea
tensorturtle <tensorturtle@gmail.com>
2024-03-14 19:20:16 +0900

    implement examples and remove scan from instance

--- bleak_fsm.py
+++ bleak_fsm.py
@@ -1,9 +1,12 @@
 import asyncio
 import time
+import logging
 
 from transitions.extensions.asyncio import AsyncMachine
 from bleak import BleakClient, BleakScanner
 from bleak.backends.device import BLEDevice
+from bleak.backends.scanner import AdvertisementData
+from bleak.exc import BleakError
 
 ### Exceptions
     
@@ -41,21 +44,51 @@ class BleakModel:
     bt_devices = {} # class variable to store the discovered devices, since we can only have one BleakScanner
     _stop_scan_event = asyncio.Event() # class variable to stop the scan
 
-    def __init__(self, scan_stale_time=120, auto_rescan=True, auto_rescan_timeout=3):
+    @classmethod
+    async def _start_scan(cls):
+        '''
+        Worker that runs the BLE scan.
+        '''
+        cls.bt_devices = {} # clear the list
+        cls._stop_scan_event.clear()  # clear the stop_scan_event
+        def detection_callback(device, advertisement_data):
+            cls.bt_devices.update({device.address: (device, advertisement_data)})
+        async with BleakScanner(detection_callback) as scanner:
+            await cls._stop_scan_event.wait() # continues to scan until stop_scan_event is set
+        return True
+
+    @classmethod
+    async def start_scan(cls):
+        '''
+        Non-blocking start of the BLE scan.
+        '''
+        loop = asyncio.get_event_loop()
+        loop.set_exception_handler(lambda loop, context: logging.error(f"Exception: {context.get('exception')}"))
+        asyncio.create_task(cls._start_scan())
+        return True
+
+    @classmethod
+    async def stop_scan(cls):
+        '''
+        Stop the BLE scan.
+        '''
+        cls._stop_scan_event.set()
+        return True
+
+    def __init__(self, logging_level=logging.WARNING):
         '''
         Args
             (int) scan_stale_time: number of seconds while scan results are considered valid for establishing connection. Must re-scan if connection attempted after this time.
             (bool) auto_rescan: If connect() attempted when scan is stale, run scan for `auto_rescan_timeout` seconds
             (int) auto_rescan_timeout: Number of seconds to scan, if auto_rescan is True. If scan isn't stale, then this value is not used and scan continues indefinitely until you call stop_scan().
         '''
+        logging.basicConfig(level=logging_level)
+        
         self.bleak_client: BleakClient = None
         self.ble_device: BLEDevice = None
+        self.advertisement_data: AdvertisementData = None
         self.target = None
         self._stop_streaming_event = asyncio.Event()
-
-        self.scan_stale_time = scan_stale_time
-        self.auto_rescan = auto_rescan
-        self.auto_rescan_timeout = auto_rescan_timeout
 
         self.wrap = lambda client: client  # Callable that sets self.wrapped_client. A callable may return identity for no wrap, or a Pycycling object which wraps a standard client. By default, an identity function.
         self.wrapped_client = None # Either identical to `self.bleak_client` (not wrapped) or custom object that represents the BLE device that presumably takes in a BleakClient, such as pycycling classes
@@ -64,19 +97,35 @@ class BleakModel:
         self.disable_notifications = None # an Async Callable must be set later that takes in a BleakClient or similar (Pycycling) object
 
         self.last_scan_time = None
-        
-    async def _bt_scan(self):        
-        def detection_callback(device, _advertisement_data):
-            BleakModel.bt_devices[device.address] = device
-        async with BleakScanner(detection_callback) as scanner:
-            await self._stop_scan_event.wait() # continues to scan until stop_scan_event is set
 
-    async def _stop_bt_scan(self):
-        self.last_scan_time = time.time()
+    async def clean_up(self):
+        '''
+        Go to Init state from all states. Call when handling exceptions or when program is exiting.
+        '''
         self._stop_scan_event.set()
+        self._stop_streaming_event.set()
+
+        if self.state == "Streaming":
+            await self.disconnect()
+        if self.state == "Connected":
+            await self.disconnect()
+        if self.state == "Scanning":
+            await self.stop_scan()
+        if self.state == "TargetSet":
+            await self.unset_target()
+        return True
+    async def _nonblocking_start_scan(self):
+        '''
+        Run _start_scan in a detached coroutine.
+        Allowing users to `await model.start_scan()` without blocking.
+        '''
+        loop = asyncio.get_event_loop()
+        loop.set_exception_handler(lambda loop, context: logging.error(f"Exception: {context.get('exception')}"))
+        asyncio.create_task(self._start_scan())
+        return True
 
     def _set_target(self, address):
-        if address in BleakModel.bt_devices:
+        if address in BleakModel.bt_devices.keys():
             self.target = address
             return True
         else:
@@ -86,46 +135,54 @@ class BleakModel:
     def _unset_target(self):
         self.target = None
 
+    async def _connect_to_device_with_timeout(self, timeout=5.0):
+        '''
+        Connect to the device with a timeout (seconds)
+        '''
+        try:
+            await asyncio.wait_for(self._connect_to_device(), timeout=timeout)
+        except asyncio.TimeoutError:
+            logging.warning(f"Timed out while connecting to {self.target}")
+            # We must disconnect so that the device is returned to the list of discovered devices
+            await self._disconnect_from_device()
+            return False
+        return True
+
     async def _connect_to_device(self):
-        # stale scan data
-        if self.last_scan_time is not None:
-            if time.time() - self.last_scan_time > self.scan_stale_time:
-                if self.auto_rescan:
-                    print("Stale scan. Automatically re-scanning before attempting connect")
-                    await self._bt_scan()
-                    await asyncio.sleep(self.auto_rescan_timeout)
-                    await self._stop_bt_scan()
-                    await self._connect_to_device() # while this looks recursive, it isn't because after one iteration, the stale time will not have been reached
-                    return True
-                else:
-                    raise StaleScanError()
-                
         if len(BleakModel.bt_devices) == 0:
             raise NoDevicesFoundError()
         try:
-            self.ble_device = (BleakModel.bt_devices.pop(self.target))# remove the device from the list to avoid connecting to it multiple times
+            self.ble_device, self.advertisement_data = (BleakModel.bt_devices.pop(self.target))# remove the device from the list to avoid connecting to it multiple times
         except:
             raise BleakFSMError(f"Bluetooth device {self.target} not found in scanned list. It could be powered off, connected to another device, or to another BleakModel.")
         self.bleak_client = BleakClient(self.ble_device) # we don't use the async context manager because we want to access the client object from the disconnect function
         try:
             connected = await self.bleak_client.connect()
             if connected:
-                print(f"Connected to {self.target}")
+                logging.info(f"Connected to {self.target}")
 
                 self.wrapped_client = self.wrap(self.bleak_client)
                     
                 return True
             else:
-                print(f"Failed to connect to {self.target}")
+                logging.warning(f"Failed to connect to {self.target}")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logging.error(f"An error occurred: {e}")
             return False
         return True
     
     async def _disconnect_from_device(self):
-        BleakModel.bt_devices[self.target] = self.ble_device # put it back in the list
+        BleakModel.bt_devices.update({self.target: (self.ble_device, self.advertisement_data)}) # put it back in the list
         await self.bleak_client.disconnect()
-        print(f"Disconnected from {self.bleak_client.address}")
+        logging.info(f"Disconnected from {self.bleak_client.address}")
+
+    async def _nonblocking_stream_from_device(self):
+        '''
+        Run _stream_from_device in a detached coroutine.
+        Allowing users to `await model.stream()` without blocking.
+        '''
+        asyncio.create_task(self._stream_from_device())
+        return True
 
     async def _stream_from_device(self):
         self._stop_streaming_event.clear()
@@ -133,7 +190,7 @@ class BleakModel:
         self.set_measurement_handler(self.wrapped_client)
         await self.enable_notifications(self.wrapped_client)
         
-        print("Started streaming")
+        logging.info("Started streaming")
 
         await self._stop_streaming_event.wait()
         return True
@@ -148,10 +205,10 @@ class BleakModel:
         try:
             await self.disable_notifications(self.wrapped_client)
 
-            print("Stopped streaming")
+            logging.info("Stopped streaming")
             return True
         except Exception as e:
-            print(f"An error occurred while stopping streaming: {e}")
+            logging.error(f"An error occurred while stopping streaming: {e}")
             return False
 
     async def _stop_stream_and_disconnect_from_device(self):
@@ -170,21 +227,7 @@ transitions = []
 
 model = BleakModel() # import this from user script
 
-machine = AsyncMachine(model, states=["Init", "Scanning", "TargetSet", "Connected", "Streaming"], transitions=transitions, initial='Init')
-
-machine.add_transition(
-    trigger="start_scan",
-    source="Init",
-    dest="Scanning",
-    after="_bt_scan"
-)
-
-machine.add_transition(
-    trigger="stop_scan",
-    source="Scanning",
-    dest="Init",
-    after="_stop_bt_scan"
-)
+machine = AsyncMachine(model, states=["Init", "TargetSet", "Connected", "Streaming"], transitions=transitions, initial='Init')
 
 machine.add_transition(
     trigger="set_target",
@@ -204,14 +247,14 @@ machine.add_transition(
     trigger="connect",
     source="TargetSet",
     dest="Connected",
-    conditions="_connect_to_device"
+    conditions="_connect_to_device_with_timeout"
 )
 
 machine.add_transition(
     trigger="stream",
     source="Connected",
     dest="Streaming",
-    after="_stream_from_device"
+    after="_nonblocking_stream_from_device"
 )
 
 machine.add_transition(
---
+++ single_hr_script_example.py
@@ -0,0 +1,74 @@
+# single_hr_script_example.py
+#
+# This is a Python script equivalent of `single_hr_example.ipynb` Jupyter notebook.
+# It streams the heart rate measurement from a Bluetooth Low Energy (BLE) heart rate device.
+# The major difference is that this script demonstrates how to handle CTRL+C signal
+# So that Bluetooth connection is properly closed before the script exits.
+
+import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger("bleak_fsm_demo")
+
+# filter out asyncio log messages
+logging.getLogger('transitions.extensions.asyncio').setLevel(logging.WARN)
+
+from bleak_fsm import machine, BleakModel, BleakFSMError
+from pycycling.heart_rate_service import HeartRateService
+
+def handle_hr_measurement(value):
+    logger.debug("Using Pycycling wrapper around BleakClient")
+    print(f"Heart Rate: {value}")
+
+async def main(model, target_name: str):
+    try:
+        logger.info("Starting BLE scan")
+        await BleakModel.start_scan()
+        await asyncio.sleep(5.0)
+        await BleakModel.stop_scan()
+        if len(BleakModel.bt_devices) == 0:
+            raise BleakFSMError("No devices found.")
+        logger.info("Scan stopped")
+        logger.debug("Discovered devices:")
+        logger.debug(BleakModel.bt_devices)
+        logger.info(f"Connecting to device named {target_name}")
+        target_address = ""
+        for address, (ble_device, advertisement_data) in BleakModel.bt_devices.items():
+            if ble_device.name == target_name:
+                target_address = address
+                logger.info(f"Found {target_name} at {target_address}")
+                break
+
+        await model.set_target(target_address)
+        await model.connect()
+        await model.stream()
+        await asyncio.sleep(5)
+        await model.disconnect()
+        await model.unset_target()
+        logger.info("Program finished. Exiting")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        await model.clean_up()
+        logger.debug(model.state)
+        logger.error("Exiting due to error")
+
+if __name__ == "__main__":
+    model = BleakModel(logging_level=logging.DEBUG)
+    machine.add_model(model)
+
+    model.wrap = lambda client: HeartRateService(client)
+    model.enable_notifications = lambda client: client.enable_hr_measurement_notifications()
+    model.disable_notifications = lambda client: client.disable_hr_measurement_notifications()
+    model.set_measurement_handler = lambda client: client.set_hr_measurement_handler(handle_hr_measurement)
+
+    try:
+        asyncio.get_event_loop().run_until_complete(main(
+            model=model,
+            target_name="WHOOPDEDOO" # Replace with the name of your heart rate device
+        ))
+    except KeyboardInterrupt: # must be caught at this top level
+        asyncio.get_event_loop().run_until_complete(model.clean_up())
+        assert model.state == "Init", "State should be 'Init' after clean_up()"
+        logger.error("\nExiting due to keyboard interrupt (CTRL+C)")
